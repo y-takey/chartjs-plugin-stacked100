787fb7a08d09e5342189892ef5c9cfa8e3d31a16
takeyuichi <y.takey@gmail.com>
2017-07-11 20:59:01 +0900

    support for chart type is (vertical) bar

--- src/index.js
+++ src/index.js
@@ -8,19 +8,21 @@
 
       var xAxes = chartInstance.options.scales.xAxes;
       var yAxes = chartInstance.options.scales.yAxes;
+      var isVertical = chartInstance.config.type === 'bar';
 
       [xAxes, yAxes].forEach(function(axes) {
         axes.forEach(function(hash) { hash.stacked = true });
-      })
-      xAxes.forEach(function(hash) { hash.ticks.min = 0; hash.ticks.max = 100 });
+      });
+      (isVertical ? yAxes : xAxes).forEach(function(hash) { hash.ticks.min = 0; hash.ticks.max = 100 });
 
       chartInstance.options.tooltips.callbacks.label = function(tooltipItem, data) {
         var datasetIndex = tooltipItem.datasetIndex,
-          index = tooltipItem.index,
-          xLabel = tooltipItem.xLabel;
+            index = tooltipItem.index,
+            xLabel = tooltipItem.xLabel,
+            yLabel = tooltipItem.yLabel;
         var datasetLabel = data.datasets[datasetIndex].label || '';
 
-        return '' + datasetLabel + ': ' + xLabel + '% (' + data.originalData[datasetIndex][index] + ')';
+        return '' + datasetLabel + ': ' + (isVertical ? yLabel : xLabel) + '% (' + data.originalData[datasetIndex][index] + ')';
       };
     },
 
@@ -28,7 +30,7 @@
       if (!pluginOptions.enable) return;
 
       var datasets = chartInstance.data.datasets;
-      var allData = datasets.map(function(dataset) { return dataset.data });
+      var allData = datasets.map(function(dataset) { return Array.from(dataset.data) });
       chartInstance.data.originalData = allData;
 
       var totals = Array.apply(null, new Array(allData[0].length)).map(function(el, i) {
