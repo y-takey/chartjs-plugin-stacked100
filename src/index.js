e1928341b3317d1eb91d12d96ccbbf3f17efcda2
Yuichiro Take <y.takey@gmail.com>
2017-08-18 14:31:30 +0900

    Recalcutate when update datasets for hidden items  #4

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-
 (function(Chart) {
   var Stacked100Plugin = {
-    id: 'stacked100',
+    id: "stacked100",
 
     beforeInit: function(chartInstance, pluginOptions) {
       if (!pluginOptions.enable) return;
@@ -10,17 +9,27 @@
       var yAxes = chartInstance.options.scales.yAxes;
 
       [xAxes, yAxes].forEach(function(axes) {
-        axes.forEach(function(hash) { hash.stacked = true });
-      })
-      xAxes.forEach(function(hash) { hash.ticks.min = 0; hash.ticks.max = 100 });
+        axes.forEach(function(hash) {
+          hash.stacked = true;
+        });
+      });
+      xAxes.forEach(function(hash) {
+        hash.ticks.min = 0;
+        hash.ticks.max = 100;
+      });
+
+      var allData = chartInstance.data.datasets.map(function(dataset) {
+        return dataset.data;
+      });
+      chartInstance.data.originalData = allData;
 
       chartInstance.options.tooltips.callbacks.label = function(tooltipItem, data) {
         var datasetIndex = tooltipItem.datasetIndex,
           index = tooltipItem.index,
           xLabel = tooltipItem.xLabel;
-        var datasetLabel = data.datasets[datasetIndex].label || '';
+        var datasetLabel = data.datasets[datasetIndex].label || "";
 
-        return '' + datasetLabel + ': ' + xLabel + '% (' + data.originalData[datasetIndex][index] + ')';
+        return "" + datasetLabel + ": " + xLabel + "% (" + data.originalData[datasetIndex][index] + ")";
       };
     },
 
@@ -28,14 +37,22 @@
       if (!pluginOptions.enable) return;
 
       var datasets = chartInstance.data.datasets;
-      var allData = datasets.map(function(dataset) { return dataset.data });
-      chartInstance.data.originalData = allData;
+      var allData = chartInstance.data.originalData;
+
+      // for excluding value of hidden item.
+      var visibles = datasets.map(function(dataset) {
+        if (!dataset._meta) return true;
+
+        return !dataset._meta[0].hidden;
+      });
 
       var totals = Array.apply(null, new Array(allData[0].length)).map(function(el, i) {
-        return allData.reduce(function(sum, data) { return sum + data[i] }, 0);
+        return allData.reduce(function(sum, data, j) {
+          return sum + data[i] * visibles[j];
+        }, 0);
       });
-      datasets.forEach(function(dataset) {
-        dataset.data = dataset.data.map(function(val, i) {
+      datasets.forEach(function(dataset, i) {
+        dataset.data = allData[i].map(function(val, i) {
           return Math.round(val * 1000 / totals[i]) / 10;
         });
       });
@@ -43,4 +60,4 @@
   };
 
   Chart.pluginService.register(Stacked100Plugin);
-}).call(this, Chart);
+}.call(this, Chart));
