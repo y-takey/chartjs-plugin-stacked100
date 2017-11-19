923cdc06d82f41c552f9285e81975c229337725d
takeyuichi <y.takey@gmail.com>
2017-11-19 09:42:00 +0900

    Updates

--- plugin.js
+++ plugin.js
@@ -9,6 +9,57 @@
     return dstAry;
   };
 
+  var setOriginalData = function(data) {
+    data.originalData = data.datasets.map(function(dataset) {
+      return cloneArray(dataset.data);
+    });
+  };
+
+  // set calculated rate (xx%) to data.calculatedData
+  var calculateRate = function(data) {
+    var visibles = data.datasets.map(function(dataset) {
+      if (!dataset._meta) return true;
+
+      for (var i in dataset._meta) {
+        return !dataset._meta[i].hidden;
+      }
+    });
+
+    var totals = Array.apply(null, new Array(data.datasets[0].data.length)).map(function(el, i) {
+      return data.datasets.reduce(function(sum, dataset, j) {
+        var key = dataset.stack;
+        if (!sum[key]) sum[key] = 0;
+        sum[key] += dataset.data[i] * visibles[j];
+
+        return sum;
+      }, {});
+    });
+
+    data.calculatedData = data.datasets.map(function(dataset, i) {
+      return dataset.data.map(function(val, i) {
+        return val ? Math.round(val * 1000 / totals[i][dataset.stack]) / 10 : 0;
+      });
+    });
+  };
+
+  var tooltipLabel = function(tooltipItem, data) {
+    var datasetIndex = tooltipItem.datasetIndex;
+    var index = tooltipItem.index;
+    var datasetLabel = data.datasets[datasetIndex].label || "";
+    var originalValue = data.originalData[datasetIndex][index];
+    var rateValue = data.calculatedData[datasetIndex][index];
+
+    return "" + datasetLabel + ": " + rateValue + "% (" + originalValue + ")";
+  };
+
+  var reflectData = function(srcData, datasets) {
+    if (!srcData) return;
+
+    srcData.forEach(function(data, i) {
+      datasets[i].data = data;
+    });
+  };
+
   var Stacked100Plugin = {
     id: "stacked100",
 
@@ -29,70 +80,21 @@
         hash.ticks.max = 100;
       });
 
-      this._setOriginalData(chartInstance);
-
-      chartInstance.options.tooltips.callbacks.label = function(tooltipItem, data) {
-        var datasetIndex = tooltipItem.datasetIndex,
-          index = tooltipItem.index,
-          xLabel = tooltipItem.xLabel,
-          yLabel = tooltipItem.yLabel;
-        var datasetLabel = data.datasets[datasetIndex].label || "";
-
-        return (
-          "" +
-          datasetLabel +
-          ": " +
-          (isVertical ? yLabel : xLabel) +
-          "% (" +
-          data.originalData[datasetIndex][index] +
-          ")"
-        );
-      };
+      chartInstance.options.tooltips.callbacks.label = tooltipLabel;
     },
 
     beforeUpdate: function(chartInstance, pluginOptions) {
       if (!pluginOptions.enable) return;
 
-      var datasets = chartInstance.data.datasets;
-      // restore
-      if (chartInstance.data.originalData) {
-        chartInstance.data.originalData.forEach(function(data, i) {
-          datasets[i].data = data;
-        });
-      } else {
-        this._setOriginalData(chartInstance);
-      }
-
-      // for excluding value of hidden item.
-      var visibles = datasets.map(function(dataset) {
-        if (!dataset._meta) return true;
-
-        for (var i in dataset._meta) {
-          return !dataset._meta[i].hidden;
-        }
-      });
-
-      var totals = Array.apply(null, new Array(datasets[0].data.length)).map(function(el, i) {
-        return datasets.reduce(function(sum, dataset, j) {
-          var key = dataset.stack;
-          if (!sum[key]) sum[key] = 0;
-          sum[key] += dataset.data[i] * visibles[j];
-
-          return sum;
-        }, {});
-      });
-      datasets.forEach(function(dataset, i) {
-        dataset.data = dataset.data.map(function(val, i) {
-          return val ? Math.round(val * 1000 / totals[i][dataset.stack]) / 10 : 0;
-        });
-      });
+      setOriginalData(chartInstance.data);
+      calculateRate(chartInstance.data);
+      reflectData(chartInstance.data.calculatedData, chartInstance.data.datasets);
     },
 
-    _setOriginalData: function(chartInstance) {
-      var allData = chartInstance.data.datasets.map(function(dataset) {
-        return cloneArray(dataset.data);
-      });
-      chartInstance.data.originalData = allData;
+    afterRender: function(chartInstance, pluginOptions) {
+      if (!pluginOptions.enable) return;
+
+      reflectData(chartInstance.data.originalData, chartInstance.data.datasets);
     }
   };
 
