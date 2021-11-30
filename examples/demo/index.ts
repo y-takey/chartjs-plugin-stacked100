8bf99980f7f85f7657f35123a0a42a7627245ebb
Yuichiro Take <y.takey@gmail.com>
2021-11-30 11:29:46 +0900

    auto correct by eslint and prettier

--- examples/demo/index.ts
+++ examples/demo/index.ts
@@ -3,10 +3,12 @@ import Chart from "chart.js/auto";
 import ChartPluginStacked100 from "../../src/index";
 Chart.register(ChartPluginStacked100);
 
-const getCanvas = (id: string): HTMLCanvasElement => document.getElementById(id) as HTMLCanvasElement
+const getCanvas = (id: string): HTMLCanvasElement =>
+  document.getElementById(id) as HTMLCanvasElement;
+
 const plugins: any = {
-  stacked100: { enable: true }
-}
+  stacked100: { enable: true },
+};
 
 new Chart(getCanvas("my-chart-1"), {
   type: "bar",
@@ -15,13 +17,13 @@ new Chart(getCanvas("my-chart-1"), {
     datasets: [
       { label: "bad", data: [5, 25], backgroundColor: "rgba(244, 143, 177, 0.6)" },
       { label: "better", data: [15, 10], backgroundColor: "rgba(255, 235, 59, 0.6)" },
-      { label: "good", data: [10, 8], backgroundColor: "rgba(100, 181, 246, 0.6)" }
-    ]
+      { label: "good", data: [10, 8], backgroundColor: "rgba(100, 181, 246, 0.6)" },
+    ],
   },
   options: {
     indexAxis: "y",
-    plugins
-  }
+    plugins,
+  },
 });
 
 new Chart(getCanvas("my-chart-2"), {
@@ -32,13 +34,13 @@ new Chart(getCanvas("my-chart-2"), {
       { label: "L1", stack: "Stack 0", data: [3, 2], backgroundColor: "rgba(244, 143, 177, 0.6)" },
       { label: "L2", stack: "Stack 0", data: [1, 1], backgroundColor: "rgba(255, 235, 59, 0.6)" },
       { label: "L1", stack: "Stack 1", data: [0, 3], backgroundColor: "rgba(100, 181, 246, 0.6)" },
-      { label: "L2", stack: "Stack 1", data: [1, 4], backgroundColor: "rgba(51, 255, 74, 0.4)" }
-    ]
+      { label: "L2", stack: "Stack 1", data: [1, 4], backgroundColor: "rgba(51, 255, 74, 0.4)" },
+    ],
   },
   options: {
     indexAxis: "y",
-    plugins
-  }
+    plugins,
+  },
 });
 
 new Chart(getCanvas("my-chart-3"), {
@@ -48,12 +50,12 @@ new Chart(getCanvas("my-chart-3"), {
     datasets: [
       { label: "L1", data: [10, 9], backgroundColor: "rgba(244, 143, 177, 0.6)" },
       { label: "L2", data: [20, 6], backgroundColor: "rgba(255, 235, 59, 0.6)" },
-      { label: "L3", data: [30, 3], backgroundColor: "rgba(100, 181, 246, 0.6)" }
-    ]
+      { label: "L3", data: [30, 3], backgroundColor: "rgba(100, 181, 246, 0.6)" },
+    ],
   },
   options: {
-    plugins
-  }
+    plugins,
+  },
 });
 
 new Chart(getCanvas("my-chart-4"), {
@@ -64,16 +66,16 @@ new Chart(getCanvas("my-chart-4"), {
       { label: "L1", fill: true, data: [1, 2, 0], backgroundColor: "rgba(244, 143, 177, 0.6)" },
       { label: "L2", fill: true, data: [1, 1, 3], backgroundColor: "rgba(255, 235, 59, 0.6)" },
       { label: "L3", fill: true, data: [1, 1, 2], backgroundColor: "rgba(100, 181, 246, 0.6)" },
-      { label: "L4", fill: true, data: [1, 3, 1], backgroundColor: "rgba(51, 255, 74, 0.4)" }
-    ]
+      { label: "L4", fill: true, data: [1, 3, 1], backgroundColor: "rgba(51, 255, 74, 0.4)" },
+    ],
   },
   options: {
     scales: {
       x: { stacked: true },
-      y: { stacked: true }
+      y: { stacked: true },
     },
-    plugins
-  }
+    plugins,
+  },
 });
 
 new Chart(getCanvas("my-chart-5"), {
@@ -81,19 +83,55 @@ new Chart(getCanvas("my-chart-5"), {
   data: {
     labels: ["2017-10-18", "2017-10-19", "2017-10-20"],
     datasets: [
-      { label: "L1", fill: true, data: [{ x: "2017-10-18", y: 1 }, { x: "2017-10-19", y: 2 }, { x: "2017-10-20", y: 0 }], backgroundColor: "rgba(244, 143, 177, 0.6)" },
-      { label: "L2", fill: true, data: [{ x: "2017-10-18", y: 1 }, { x: "2017-10-19", y: 1 }, { x: "2017-10-20", y: 3 }], backgroundColor: "rgba(255, 235, 59, 0.6)" },
-      { label: "L3", fill: true, data: [{ x: "2017-10-18", y: 1 }, { x: "2017-10-19", y: 1 }, { x: "2017-10-20", y: 2 }], backgroundColor: "rgba(100, 181, 246, 0.6)" },
-      { label: "L4", fill: true, data: [{ x: "2017-10-18", y: 1 }, { x: "2017-10-19", y: 3 }, { x: "2017-10-20", y: 1 }], backgroundColor: "rgba(51, 255, 74, 0.4)" }
-    ]
+      {
+        label: "L1",
+        fill: true,
+        data: [
+          { x: "2017-10-18", y: 1 },
+          { x: "2017-10-19", y: 2 },
+          { x: "2017-10-20", y: 0 },
+        ],
+        backgroundColor: "rgba(244, 143, 177, 0.6)",
+      },
+      {
+        label: "L2",
+        fill: true,
+        data: [
+          { x: "2017-10-18", y: 1 },
+          { x: "2017-10-19", y: 1 },
+          { x: "2017-10-20", y: 3 },
+        ],
+        backgroundColor: "rgba(255, 235, 59, 0.6)",
+      },
+      {
+        label: "L3",
+        fill: true,
+        data: [
+          { x: "2017-10-18", y: 1 },
+          { x: "2017-10-19", y: 1 },
+          { x: "2017-10-20", y: 2 },
+        ],
+        backgroundColor: "rgba(100, 181, 246, 0.6)",
+      },
+      {
+        label: "L4",
+        fill: true,
+        data: [
+          { x: "2017-10-18", y: 1 },
+          { x: "2017-10-19", y: 3 },
+          { x: "2017-10-20", y: 1 },
+        ],
+        backgroundColor: "rgba(51, 255, 74, 0.4)",
+      },
+    ],
   },
   options: {
     scales: {
       x: { stacked: true },
-      y: { stacked: true }
+      y: { stacked: true },
     },
-    plugins
-  }
+    plugins,
+  },
 });
 
 new Chart(getCanvas("my-chart-6"), {
@@ -101,13 +139,34 @@ new Chart(getCanvas("my-chart-6"), {
   data: {
     labels: ["Foo", "Bar"],
     datasets: [
-      { label: "bad", data: [{ x: 5, y: "Foo" }, { x: 25, y: "Bar" }], backgroundColor: "rgba(244, 143, 177, 0.6)" },
-      { label: "better", data: [{ x: 15, y: "Foo" }, { x: 10, y: "Bar" }], backgroundColor: "rgba(255, 235, 59, 0.6)" },
-      { label: "good", data: [{ x: 10, y: "Foo" }, { x: 8, y: "Bar" }], backgroundColor: "rgba(100, 181, 246, 0.6)" }
-    ]
+      {
+        label: "bad",
+        data: [
+          { x: 5, y: "Foo" },
+          { x: 25, y: "Bar" },
+        ],
+        backgroundColor: "rgba(244, 143, 177, 0.6)",
+      },
+      {
+        label: "better",
+        data: [
+          { x: 15, y: "Foo" },
+          { x: 10, y: "Bar" },
+        ],
+        backgroundColor: "rgba(255, 235, 59, 0.6)",
+      },
+      {
+        label: "good",
+        data: [
+          { x: 10, y: "Foo" },
+          { x: 8, y: "Bar" },
+        ],
+        backgroundColor: "rgba(100, 181, 246, 0.6)",
+      },
+    ],
   },
   options: {
     indexAxis: "y",
-    plugins
-  }
+    plugins,
+  },
 });
--- src/plugin.ts
+++ src/plugin.ts
@@ -1,12 +1,17 @@
 import { Chart, ChartType, TooltipCallbacks, TooltipItem } from "chart.js";
 
 import { dataValue, setOriginalData, round, getPrecision } from "./utils";
-import { ExtendedChartData, ExtendedPlugin } from "./types"
+import { ExtendedChartData, ExtendedPlugin } from "./types";
 
-const calculateRate = (data: ExtendedChartData, visibles: number[], isHorizontal: boolean, precision: number) => {
+const calculateRate = (
+  data: ExtendedChartData,
+  visibles: number[],
+  isHorizontal: boolean,
+  precision: number,
+) => {
   const datasetDataLength = data?.datasets?.[0]?.data?.length || 0;
 
-  const totals = [...(new Array(datasetDataLength))].map((el, i) => {
+  const totals = [...new Array(datasetDataLength)].map((el, i) => {
     return data.datasets.reduce((sum, dataset, j) => {
       const key = dataset.stack;
       if (!sum[key]) sum[key] = 0;
@@ -36,59 +41,68 @@ const tooltipLabel = (isHorizontal: boolean): TooltipCallbacks<ChartType>["label
     const value = dataValue(originalValue, isHorizontal);
 
     return `${datasetLabel}: ${rateValue}% (${value})`;
-  }
+  };
 };
 
 const reflectData = (srcData: any[], datasets: ExtendedChartData["datasets"]) => {
   if (!srcData) return;
 
-  srcData.forEach((data, i)  => {
+  srcData.forEach((data, i) => {
     datasets[i].data = data;
   });
 };
 
 const isHorizontalChart = (chartInstance: Chart) => {
   return chartInstance.options.indexAxis === "y";
-}
-
+};
 
 export const beforeInit: ExtendedPlugin["beforeInit"] = (chartInstance, args, pluginOptions) => {
   if (!pluginOptions.enable) return;
-  const { replaceTooltipLabel = true } = pluginOptions
+  const { replaceTooltipLabel = true } = pluginOptions;
 
   const isHorizontal = isHorizontalChart(chartInstance);
-  const targetAxis = isHorizontal ? "x" : "y"
+  const targetAxis = isHorizontal ? "x" : "y";
   const hasNegative = chartInstance.data.datasets.some((dataset) => {
     return dataset.data.some((value) => value < 0);
   });
-  ["x", "y"].forEach(axis => {
-    const tickOption = axis === targetAxis ? { min: hasNegative ? -100 : 0, max: 100 } : {}
-    const scaleOption = { stacked: true, ...tickOption, ...chartInstance.options.scales[axis] }
-    chartInstance.options.scales[axis] = scaleOption
-  })
+  ["x", "y"].forEach((axis) => {
+    const tickOption = axis === targetAxis ? { min: hasNegative ? -100 : 0, max: 100 } : {};
+    const scaleOption = {
+      stacked: true,
+      ...tickOption,
+      ...chartInstance.options.scales[axis],
+    };
+    chartInstance.options.scales[axis] = scaleOption;
+  });
 
   // Replace tooltips
   if (!replaceTooltipLabel) return;
 
   chartInstance.options.plugins.tooltip.callbacks.label = tooltipLabel(isHorizontal);
-}
+};
 
-export const beforeUpdate: ExtendedPlugin["beforeUpdate"] = (chartInstance, _args, pluginOptions) =>{
+export const beforeUpdate: ExtendedPlugin["beforeUpdate"] = (
+  chartInstance,
+  _args,
+  pluginOptions,
+) => {
   if (!pluginOptions.enable) return;
 
-  const data = chartInstance.data as ExtendedChartData
+  const data = chartInstance.data as ExtendedChartData;
 
   setOriginalData(data);
-  const visibles = data.datasets.map((_dataset, i) => chartInstance.getDatasetMeta(i)?.hidden ? 0 : 1 );
+  const visibles = data.datasets.map((_dataset, i) =>
+    chartInstance.getDatasetMeta(i)?.hidden ? 0 : 1,
+  );
   const precision = getPrecision(pluginOptions);
   data.calculatedData = calculateRate(data, visibles, isHorizontalChart(chartInstance), precision);
   reflectData(data.calculatedData, data.datasets);
-}
+};
 
-export const afterUpdate: ExtendedPlugin["afterUpdate"]  = (chartInstance, _args, pluginOptions) => {
+export const afterUpdate: ExtendedPlugin["afterUpdate"] = (chartInstance, _args, pluginOptions) => {
   if (!pluginOptions.enable) return;
 
-  const data = chartInstance.data as ExtendedChartData
+  const data = chartInstance.data as ExtendedChartData;
 
   reflectData(data.originalData, data.datasets);
-}
+};
