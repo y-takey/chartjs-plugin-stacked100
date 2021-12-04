6820df2f8fb6d1a1b30f1d03fce652165c930c1e
Yuichiro Take <y.takey@gmail.com>
2021-12-04 11:12:38 +0900

    update the plugin in demo

--- examples/demo-react/src/ExampleChart.tsx
+++ examples/demo-react/src/ExampleChart.tsx
@@ -1,6 +1,5 @@
-import { ChartOptions } from 'chart.js';
-import { Chart, Bar } from 'react-chartjs-2';
-import ChartjsPluginStacked100 from 'chartjs-plugin-stacked100';
+import { Chart, Bar } from "react-chartjs-2";
+import ChartjsPluginStacked100 from "chartjs-plugin-stacked100";
 
 Chart.register(ChartjsPluginStacked100);
 
@@ -12,15 +11,17 @@ const ExampleChart = () => (
         datasets: [
           { label: "bad", data: [5, 25], backgroundColor: "rgba(244, 143, 177, 0.6)" },
           { label: "better", data: [15, 10], backgroundColor: "rgba(255, 235, 59, 0.6)" },
-          { label: "good", data: [10, 8], backgroundColor: "rgba(100, 181, 246, 0.6)" }]
+          { label: "good", data: [10, 8], backgroundColor: "rgba(100, 181, 246, 0.6)" },
+        ],
       }}
       options={{
         indexAxis: "y",
         plugins: {
-          stacked100: { enable: true }
-        }
-      } as ChartOptions} />
+          stacked100: { enable: true },
+        },
+      }}
+    />
   </div>
 );
 
-export default ExampleChart
+export default ExampleChart;
